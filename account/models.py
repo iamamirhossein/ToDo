afcd6cd378d3e0b7485d2d1fadfdae8a0b889d6d
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-03-02 23:29:58 +0330

    feat: :fire: Create model Profile

--- account/models.py
+++ account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    nick_name = models.CharField(max_length=64)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='img/profiles/', null=True)

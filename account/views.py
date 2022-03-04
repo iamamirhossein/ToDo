3806d167029490eb64c7959c3cc3d726f27b8b95
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-03-04 22:26:10 +0330

    feat: :fire: added ProfileViewSet

--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.filter(is_active=True)
+    permission_classes = [IsAuthenticated]
+    serializer_class = ProfileSerializer

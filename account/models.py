3086c9537744a0ce23ef4339103859100bcd14d9
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-03-04 21:50:23 +0330

    feat: :fire:  Add field is_active to profile

--- account/models.py
+++ account/models.py
@@ -6,3 +6,4 @@ class Profile(models.Model):
     nick_name = models.CharField(max_length=64)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='img/profiles/', null=True)
+    is_active = models.BooleanField(default=True)

5e464b2abde9bf68f460e8a42d5e8d0d1382392b
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-03-05 18:22:51 +0330

    refactor: :bug: used exist() on query

--- utils/permissions.py
+++ utils/permissions.py
@@ -5,4 +5,4 @@ from account.models import Profile
 class IsOwnerOfProfilePermission(BasePermission):
     def has_permission(self, request, view):
         profile_id = view.kwargs.get('pk')
-        return Profile.objects.filter(pk=profile_id, user=request.user)
+        return Profile.objects.filter(pk=profile_id, user=request.user).exists()

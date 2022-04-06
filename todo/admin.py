7a444f78f041d2a01f28bd49a1a2e89c948cb62a
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-04-06 21:43:17 +0430

    feat: :fire: added ItemAdmin and registered on site

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'created_at', 'modified_at',)
+    list_filter = ('status',)
+
+
+admin.site.register(Item, ItemAdmin)

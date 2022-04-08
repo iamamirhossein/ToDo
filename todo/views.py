c0560b5de2d3de62583cc28b9cc15ef3f660e7fc
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-04-08 19:13:06 +0430

    feat: :zap: added ItemViewSet to ToDO app

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,18 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Item
+from .serializers import ItemSerializer
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+    def get_permissions(self):
+        if self.action == "destroy":
+            self.permission_classes = [IsAdminUser]
+        else:
+            self.permission_classes = [IsAuthenticated]
+
+        return super().get_permissions()
+

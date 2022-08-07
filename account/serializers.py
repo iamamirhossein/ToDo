cbc9c7a24df0882610ee4456b9d4f4e113a61c5e
imamirhossein <amirhosseinkhoshdel@gmail.com>
2022-08-07 17:46:42 +0430

    refactor: :art: use get_or_create instead of create for Profile createing

--- account/serializers.py
+++ account/serializers.py
@@ -4,9 +4,11 @@ from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    is_created = serializers.CharField(read_only=True)
+
     class Meta:
         model = Profile
-        fields = ('nick_name', 'avatar',)
+        fields = ('nick_name', 'avatar', 'is_created',)
 
     def create(self, validated_data):
         user = None
@@ -15,7 +17,8 @@ class ProfileSerializer(serializers.ModelSerializer):
             user = request.user
         validated_data["user"] = user
         try:
-            Profile(**validated_data).save()
-            return Profile(**validated_data)
+            instance, is_created = Profile.objects.get_or_create(**validated_data)
+            instance.is_created = is_created
+            return instance
         except Exception as ex:
             raise exceptions.ValidationError(str(ex))
